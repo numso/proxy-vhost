86b5a341d73e971500324446284c3841973e9eb4
numso <dosmun@gmail.com>
2014-06-08 13:28:53 -0600

    aoeu

--- app.js
+++ app.js
@@ -3,106 +3,65 @@
 
 // TODO:: Check for body and cookie passing in
 
-var express = require('express'),
-    request = require('request'),
-     config = require('config'),
-         fs = require('fs'),
-          _ = require('underscore');
+var express = require('express');
+var request = require('request');
+var      fs = require('fs');
+var       _ = require('underscore');
 
-var port = process.env.PORT || config.port || process.argv[2] || 3000;
+var port = process.env.PORT || 4000;
 
-var cc = config.couch;
-var couchURL = 'http://' + cc.host + ':' + cc.port + '/' + cc.db;
+var domains = [
+  'lanaosmunmusic.com',
+  'osmun.net',
+  'antherion.com'
+];
 
-var domains = config.domains;
-var subs;
+var subs = {
+  'lanaosmunmusic.com': {
+    'www': 'localhost:3000'
+  },
+  'osmun.net': {
+    'www': 'localhost:3010'
+  },
+  'antherion.com': {
+    'www': 'localhost:3020'
+  },
+  'idbcreative.com': {
+    'www': 'localhost:3030'
+  }
+};
 
-getAllSubdomains(init);
+var app = express();
+app.use(express.logger('dev'));
+app.use(express.methodOverride());
 
-function init(err) {
-  if (err) console.error(err);
+_.each(domains, function (domain) {
+  var fn = proxy(domain);
+  app.use(express.vhost(domain, fn));
+  app.use(express.vhost('*.' + domain, fn));
+});
 
-  var app = express();
-  app.use(express.logger('dev'));
-  app.use(express.methodOverride());
-
-  app.use(refresher);
-
-  _.each(domains, function (domain) {
-    var fn = proxy(domain);
-    app.use(express.vhost(domain, fn));
-    app.use(express.vhost('*.' + domain, fn));
-  });
-
-  app.use(show404);
-  app.use(express.errorHandler());
-  app.listen(port);
-}
+app.use(show404);
+app.use(express.errorHandler());
+app.listen(port);
 
 function proxy(domain) {
   return function (req, res, next) {
     var sub = req.subdomains || [];
     sub = sub.reverse().join('.');
 
-    if (!subs[domain][sub] && sub == '') {
-      sub = 'www';
-    }
+    if (!subs[domain][sub] && sub === '') sub = 'www';
 
     if (!subs[domain][sub]) return next();
 
     var url = 'http://' + subs[domain][sub] + req.url;
-    var test = request(url);
-    req.pipe(test).pipe(res);
-    test.on('error', function (err) {
+    var newUrl = request(url);
+    req.pipe(newUrl).pipe(res);
+    newUrl.on('error', function (err) {
       console.error(err);
       next();
     });
   };
-}
-
-function refresher(req, res, next) {
-  if (req.subdomains[0] !== 'refresh'
-    || req.query.token !== config.token)
-    return next();
-
-  getAllSubdomains(function (err) {
-    if (err) {
-      return res.send({
-        success: false,
-        err: err
-      });
-    }
-
-    res.send({
-      success: true
-    });
-  });
-}
-
-function getAllSubdomains(cb) {
-  var newSubs = {};
-  var done = _.after(domains.length, function () {
-    subs = newSubs;
-    console.log(subs);
-    cb();
-  });
-
-  _.each(domains, function (domain) {
-    newSubs[domain] = {};
-    request(couchURL + '/_design/_view/_view/getDomain?key="' + domain + '"',
-      function (err, resp, body) {
-        if (err) {
-          return cb(err);
-        } else {
-          body = JSON.parse(body);
-          _.each(body.rows, function (obj) {
-            newSubs[domain][(obj.value.sub || '')] = obj.value.host;
-          });
-        }
-        done();
-      }
-    );
-  });
 }
 
 function show404(req, res) {
